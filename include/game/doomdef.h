57f68d0010a103493180e8b9ca28f261d7f95ce2
Mateusz Szczukiewicz <szcmat6277@gmail.com>
2026-03-25 18:13:01 +0100

    feat: implement doom-style map structures

---
+++ include/game/doomdef.h
@@ -0,0 +1,18 @@
+#ifndef PWDOOM_DOOMDEF_H
+#define PWDOOM_DOOMDEF_H
+
+#include <stdint.h>
+
+#define ML_BLOCKING      1
+#define ML_BLOCKMONSTERS 2
+#define ML_TWOSIDED      4
+#define ML_DONTPEGGOP    8
+#define ML_DONTPEGBOTTOM 16
+#define ML_SECRET        32
+#define ML_SOUNDBLOCK    64
+#define ML_DONTDRAW      128
+#define ML_MAPPED        256
+
+#define NO_INDEX 0xFFFF
+
+#endif
--- include/game/map.h
+++ include/game/map.h
@@ -1,16 +1,63 @@
 #ifndef PWDOOM_MAP_H
 #define PWDOOM_MAP_H
 
+#include <stdint.h>
 #include <stdbool.h>
 
-#define MAP_WIDTH 10
-#define MAP_HEIGHT 10
+#define MAX_VERTICES 1024
+#define MAX_SECTORS  256
+#define MAX_SIDEDEFS 2048
+#define MAX_LINEDEFS 2048
 
-typedef int LevelMap[MAP_HEIGHT][MAP_WIDTH];
+typedef struct {
+    float x;
+    float y;
+} Vertex;
 
-bool can_move(const LevelMap *map, float x, float z);
+typedef struct {
+    float floor_height;
+    float ceiling_height;
+    uint16_t floor_texture;
+    uint16_t ceiling_texture;
+    uint16_t light_level;
+    uint16_t type;
+    uint16_t tag;
+} Sector;
 
-void map_init(LevelMap map);
+typedef struct {
+    int16_t x_offset;
+    int16_t y_offset;
+    uint16_t upper_texture;
+    uint16_t lower_texture;
+    uint16_t middle_texture;
+    uint16_t sector;
+} Sidedef;
+
+typedef struct {
+    uint16_t start_vertex;
+    uint16_t end_vertex;
+    uint16_t flags;
+    uint16_t front_sidedef;
+    uint16_t back_sidedef;
+} Linedef;
+
+typedef struct {
+    Vertex vertices[MAX_VERTICES];
+    uint16_t vertex_count;
+
+    Sector sectors[MAX_SECTORS];
+    uint16_t sector_count;
+
+    Sidedef sidedefs[MAX_SIDEDEFS];
+    uint16_t sidedef_count;
+
+    Linedef linedefs[MAX_LINEDEFS];
+    uint16_t linedef_count;
+} LevelMap;
+
+float get_floor_height(const LevelMap *map, float x, float z);
+
+void map_init(LevelMap *map);
 
 void map_render(const LevelMap *map);
 
